0759f57df22babaadd6169e1b67ae964c1dc428a
Darensii <darensiiii@gmail.com>
2024-05-14 10:45:46 +0800

    Latest Version
    Latest version

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [response, setResponse] = useState("Hi, welcome to UTS-calendar Website. How can I help you?");
+  const [value, setValue] = useState("");
+  
+  const onChange = (e) => setValue(e.target.value);
+
+  const handleSubmit = async () => {
+    const response = await axios.post("http://localhost:3005/chatbot", {
+      question: value,
+    });
+    setResponse(response.data);
+  };
+
+  return (
+    <>
+      <div className="top">
+        <img src="./src/UTS_Logo.png" width="100" />
+      </div>
+
+      {/* Background with styled div */}
+      <div className="background" style={{ backgroundImage: `url("https://github-production-user-asset-6210df.s3.amazonaws.com/162389957/315539928-32a08f60-4546-4b49-9c13-9f76561a90fa.jpg")` }}>
+        <p>UTS-Calendar AI Bot: {response}</p>
+      </div>
+
+      <div className="text_box">
+        <input
+          placeholder="Please type something..."
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+        <div id="submit" onClick={handleSubmit}>
+          ➢
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
---
+++ src/script.js
@@ -0,0 +1,5 @@
+const redirectButton = document.querySelector('.redirect-button');
+
+redirectButton.addEventListener('click', () => {
+  window.location.href = 'https://www.uts.edu.my/#'; // Replace with your desired URL
+});
