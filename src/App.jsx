e49a1c39fd2baab21c76c9b3abc6b5b4b3fb7fee
Darensii <darensiiii@gmail.com>
2024-05-21 20:23:03 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const onChange = (e) => setValue(e.target.value);
 
   const handleSubmit = async () => {
-    const response = await axios.post("https://byteblaster.onrender.com/chatbox", {
+    const response = await axios.post("https://byteblaster.onrender.com/", {
       question: value,
     });
     setResponse(response.data);
