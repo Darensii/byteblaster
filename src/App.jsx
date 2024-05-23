7709983e48bdf8516839e0e96e3dae767859fa41
Darensii <darensiiii@gmail.com>
2024-05-23 22:33:33 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import UTSLogo from "./UTS_Logo.png";
-import MessageDisplay from "./MessageDisplay";
 
 const App = () => {
   const [response, setResponse] = useState("Hi, welcome to UTS-calendar Website. How can I help you?");
@@ -11,10 +10,10 @@ const App = () => {
 
   const handleSubmit = async () => {
     try {
-      const res = await axios.post("/chatbot", {
+      const response = await axios.post("/chatbot", {
         question: value,
       });
-      setResponse(res.data);
+      setResponse(response.data);
     } catch (error) {
       console.error("Error:", error);
       setResponse("An error occurred while processing your request.");
@@ -24,38 +23,31 @@ const App = () => {
   return (
     <>
       <div className="top">
-        <img src={UTSLogo} width="100" alt="UTS Logo" />
+      <img src={UTSLogo} width="100" alt="UTS Logo" />
       </div>
       
-      <BackgroundWithAI response={response} value={value} onChange={onChange} handleSubmit={handleSubmit} />
+      {/* Background with styled div */}
+      <div className="background" style={{ backgroundImage: `url("https://github-production-user-asset-6210df.s3.amazonaws.com/162389957/315539928-32a08f60-4546-4b49-9c13-9f76561a90fa.jpg")` }}>
+        <p>UTS-Calendar AI Bot: {response}</p>
+        <div className="user_question">
+        <p>User:</p>
+        <p>{value}</p>
+      </div>
+      </div>
+      <div className="text_box">
+        
+        <input
+          placeholder="Please type something..."
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+        <div id="submit" onClick={handleSubmit}>
+          ➢
+        </div>
+      </div>
     </>
   );
 };
 
-const BackgroundWithAI = ({ response, value, onChange, handleSubmit }) => {
-  return (
-    <div className="background" style={{ backgroundImage: `url("https://github-production-user-asset-6210df.s3.amazonaws.com/162389957/315539928-32a08f60-4546-4b49-9c13-9f76561a90fa.jpg")` }}>
-      <MessageDisplay messageType="bot" content={response} />
-      <MessageDisplay messageType="user" content={value} />
-      <TextBox value={value} onChange={onChange} handleSubmit={handleSubmit} />
-    </div>
-  );
-};
-
-const TextBox = ({ value, onChange, handleSubmit }) => {
-  return (
-    <div className="text_box">
-      <input
-        placeholder="Please type something..."
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-      <div id="submit" onClick={handleSubmit}>
-        ➢
-      </div>
-    </div>
-  );
-};
-
-export default MessageDisplay;
+export default App;
