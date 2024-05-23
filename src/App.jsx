43b5e7b05d6d6b8c2d5c8fbeb5a881f9452c6d4e
Darensii <darensiiii@gmail.com>
2024-05-23 21:24:57 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,30 +23,40 @@ const App = () => {
   return (
     <>
       <div className="top">
-      <img src={UTSLogo} width="100" alt="UTS Logo" />
+        <img src={UTSLogo} width="100" alt="UTS Logo" />
       </div>
       
-      {/* Background with styled div */}
-      <div className="background" style={{ backgroundImage: `url("https://github-production-user-asset-6210df.s3.amazonaws.com/162389957/315539928-32a08f60-4546-4b49-9c13-9f76561a90fa.jpg")` }}>
-        <p>UTS-Calendar AI Bot: {response}</p>
-        <div className="user_question">
+      <BackgroundWithAI response={response} value={value} onChange={onChange} handleSubmit={handleSubmit} />
+    </>
+  );
+};
+
+const BackgroundWithAI = ({ response, value, onChange, handleSubmit }) => {
+  return (
+    <div className="background" style={{ backgroundImage: `url("https://github-production-user-asset-6210df.s3.amazonaws.com/162389957/315539928-32a08f60-4546-4b49-9c13-9f76561a90fa.jpg")` }}>
+      <p>UTS-Calendar AI Bot: {response}</p>
+      <div className="user_question">
         <p>User:</p>
         <p>{value}</p>
       </div>
+      <TextBox value={value} onChange={onChange} handleSubmit={handleSubmit} />
+    </div>
+  );
+};
+
+const TextBox = ({ value, onChange, handleSubmit }) => {
+  return (
+    <div className="text_box">
+      <input
+        placeholder="Please type something..."
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+      <div id="submit" onClick={handleSubmit}>
+        ➢
       </div>
-      <div className="text_box">
-        
-        <input
-          placeholder="Please type something..."
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-        <div id="submit" onClick={handleSubmit}>
-          ➢
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
